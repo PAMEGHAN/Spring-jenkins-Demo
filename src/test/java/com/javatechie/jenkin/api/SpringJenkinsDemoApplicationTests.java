85fb0e2008d458407abdf006dd20935b1cce3ac2
pameghan <parepally.meghana@capgemini.com>
2022-06-29 14:58:56 +0530

    second commit

--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsDemoApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsDemoApplicationTests.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@Runwith(SpringRunner.class)
 @SpringBootTest
 class SpringJenkinsDemoApplicationTests {
 
